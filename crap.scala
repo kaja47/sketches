ac2d3fc782e847c0a2d2e7895707794d19074fa7
K <kaja47@k47.cz>
2015-06-27 07:02:34 +0200

    add IntFreqMap

--- crap.scala
+++ crap.scala
@@ -49,3 +49,192 @@ object crap {
 		}
 	}
 }
+
+
+
+/** Frequency map intended for getting top-K elements from heavily skewed datasets.
+  * It's precise if at most K elements have higher frequency than $freqThreshold.
+  **/
+final class IntFreqMap(initialSize: Int = 32, loadFactor: Double = 0.3, freqThreshold: Int = 16) {
+	import java.lang.Integer.highestOneBit
+
+	require(loadFactor > 0.0 && loadFactor < 1.0)
+	require(freqThreshold > 1)
+
+	private[this] var capacity = math.max(higherPowerOfTwo(initialSize), 16)
+	private[this] var _size = 0
+	private[this] var maxSize = (capacity * loadFactor).toInt
+	private[this] val realFreqThreshold = higherPowerOfTwo(freqThreshold)
+
+	private[this] var keys: Array[Int] = new Array[Int](capacity)
+	private[this] var freq: Array[Int] = new Array[Int](capacity) // frequency of corresponding key
+	private[this] val lowFreqs: Array[Int] = new Array[Int](realFreqThreshold+1) // frequency of low frequencies
+
+
+	def size = _size
+
+	def += (k: Int, count: Int = 1): this.type = {
+		assert(count > 0)
+
+		val i = findIdx(k)
+
+		val oldFreq = freq(i)
+
+		if (oldFreq == 0) {
+			_size += 1
+		}
+
+		keys(i) = k
+		freq(i) += count
+
+		updateLowFrequency(oldFreq, freq(i))
+
+		if (_size > maxSize) {
+			grow()
+		}
+
+		this
+	}
+
+
+	def ++= (ks: Array[Int], count: Int): this.type = {
+		var i = 0
+		while (i < ks.length) {
+			this += (ks(i), count)
+			i += 1
+		}
+		this
+	}
+
+	def get(k: Int): Int = {
+		val i = findIdx(k)
+		if (freq(i) > 0) {
+			freq(i)
+		} else {
+			throw new java.util.NoSuchElementException("key not found: "+k)
+		}
+	}
+
+	def clear(): this.type = {
+		this._size = 0
+
+		var i = 0
+		while (i < freq.length) {
+			keys(i) = 0
+			freq(i) = 0
+			i += 1
+		}
+
+		var j = 0
+		while (j < lowFreqs.length) {
+			lowFreqs(j) = 0
+			j += 1
+		}
+
+		this
+	}
+
+	def iterator = keys.iterator zip freq.iterator filter { case (k, f) => f > 0 }
+
+	def toArray: Array[(Int, Int)] = {
+		val res = new Array[(Int, Int)](_size)
+		var i, j = 0
+		while (i < capacity) {
+			if (freq(i) > 0) {
+				res(j) = (keys(i), freq(i))
+				j += 1
+			}
+			i += 1
+		}
+		res
+	}
+
+	override def toString = iterator.mkString("IntFreqMap(", ",", ")")
+
+
+
+	private def updateLowFrequency(oldFreq: Int, newFreq: Int): Unit = {
+		val mask = realFreqThreshold - 1
+		lowFreqs(if (oldFreq < realFreqThreshold) oldFreq & mask else realFreqThreshold) -= 1
+		lowFreqs(if (newFreq < realFreqThreshold) newFreq & mask else realFreqThreshold) += 1
+	}
+
+	private def higherPowerOfTwo(x: Int) =
+		highestOneBit(x) << (if (highestOneBit(x) == x) 0 else 1)
+
+	private def findIdx(k: Int) = {
+		val mask = capacity - 1
+		val pos = k & mask
+		var i = pos
+		while (freq(i) != 0 && keys(i) != k) {
+			i = (i + 1) & mask
+		}
+		i
+	}
+
+	/** used to add elements into resized arrays in grow() method */
+	private def growset(k: Int, count: Int): Unit = {
+		val i = findIdx(k)
+		keys(i) = k
+		freq(i) = count
+	}
+
+	private def grow(): Unit = {
+		val oldKeys = keys
+		val oldFreq = freq
+
+		this.capacity *= 2
+		this.maxSize = (this.capacity * loadFactor).toInt
+		this.keys = new Array[Int](this.capacity)
+		this.freq = new Array[Int](this.capacity)
+
+		var i = 0
+		while (i < oldKeys.length) {
+			if (oldFreq(i) > 0) {
+				this.growset(oldKeys(i), oldFreq(i))
+			}
+			i += 1
+		}
+	}
+
+	/** @return k most frequent elements, wihout corresponding frequency, not sorted */
+	def topK(k: Int): Array[Int] = {
+		require(k > 0)
+
+		val realK = math.min(k, size)
+		val res = new Array[Int](realK)
+
+		var i = realFreqThreshold+1
+		var freqSum = 0
+		while (i > 1 && freqSum < realK) {
+			i -= 1
+			freqSum += lowFreqs(i)
+		}
+
+		val smallestFreq = i
+		val wholeFreq    = i+1 // TODO: what if size of wholeFreq is bigger than realK, then method produces imprecise results
+
+		var resIdx = 0
+
+		var mapIdx = 0
+		while (mapIdx < freq.length && resIdx < res.length) {
+			if (freq(mapIdx) >= wholeFreq) {
+				res(resIdx) = keys(mapIdx)
+				resIdx += 1
+			}
+			mapIdx += 1
+		}
+
+		mapIdx = 0
+		while (mapIdx < freq.length && resIdx < res.length) {
+			if (freq(mapIdx) < wholeFreq && freq(mapIdx) >= smallestFreq) {
+				res(resIdx) = keys(mapIdx)
+				resIdx += 1
+			}
+			mapIdx += 1
+		}
+
+		res
+	}
+
+}
