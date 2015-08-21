2491af68602d89101d17156210b0d41dc8028074
K <kaja47@k47.cz>
2015-08-21 02:27:27 +0200

    StringIntDictionary: change internal representation
    This change allows empty strings and strings longer that 255 characters to
    be used as keys and makes possible to implement methods for removing
    key-value pairs.

--- StringIntDictionary.scala
+++ StringIntDictionary.scala
@@ -66,7 +66,7 @@ class StringIntDictionary(initialCapacity: Int = 1024, loadFactor: Double = 0.45
 
   def iterator: Iterator[(String, Int)] = Iterator.tabulate(capacity) { i =>
     val pos = i * segmentLength
-    if (stringLength(assoc, pos) == 0) null
+    if (!isAlive(assoc, pos)) null
     else (makeString(assoc, pos), payload(assoc, pos).toInt)
   } filter (_ != null)
 
@@ -105,7 +105,7 @@ class StringLongDictionary(initialCapacity: Int = 1024, loadFactor: Double = 0.4
 
   def iterator: Iterator[(String, Long)] = Iterator.tabulate(capacity) { i =>
     val pos = i * segmentLength
-    if (stringLength(assoc, pos) == 0) null
+    if (!isAlive(assoc, pos)) null
     else (makeString(assoc, pos), payload(assoc, pos))
   } filter (_ != null)
 
@@ -130,7 +130,7 @@ class StringSet(initialCapacity: Int = 1024, loadFactor: Double = 0.45)
 
   def iterator: Iterator[String] = Iterator.tabulate(capacity) { i =>
     val pos = i * segmentLength
-    if (stringLength(assoc, pos) == 0) null
+    if (!isAlive(assoc, pos)) null
     else makeString(assoc, pos)
   } filter (_ != null)
 
@@ -147,6 +147,7 @@ abstract class StringDictionaryBase(initialCapacity: Int = 1024, val loadFactor:
 
   protected var capacity = max(higherPowerOfTwo(initialCapacity), 16)
   protected var occupied = 0
+  protected var removed  = 0
   protected var maxOccupied = min(capacity * loadFactor toInt, capacity - 1)
   protected var charsTop = 0
 
@@ -156,12 +157,16 @@ abstract class StringDictionaryBase(initialCapacity: Int = 1024, val loadFactor:
   protected val defaultValue = 0L
 
   // debug information
-  var _puts = 0L
-  var _gets = 0L
-  var _probes = 0L
+  var _puts       = 0L
+  var _gets       = 0L
+  var _dels       = 0L
+  var _fastdels   = 0L
+  var _reputs     = 0L
+  var _probes     = 0L
   var _growProbes = 0L
-  def _byteSize = assoc.length * 4 + chars.length * 2
-  def _capacity = capacity
+  var _leak = 0L
+  def _byteSize   = assoc.length * 4 + chars.length * 2
+  def _capacity   = capacity
 
   private def higherPowerOfTwo(x: Int) =
     highestOneBit(x) << (if (highestOneBit(x) == x) 0 else 1)
@@ -201,15 +206,15 @@ abstract class StringDictionaryBase(initialCapacity: Int = 1024, val loadFactor:
 
 
   def contains(str: CharSequence): Boolean =
-    stringLength(assoc, findPos(str, tryInline(str))) > 0
+    isAlive(assoc, findPos(str, tryInline(str)))
 
-  def size = occupied
+  def size = occupied - removed
 
 
   // internals
 
   protected def getInternal(pos: Int, defaultValue: Long): Long =
-    if (stringLength(assoc, pos) > 0) payload(assoc, pos) else defaultValue
+    if (isAlive(assoc, pos)) payload(assoc, pos) else defaultValue
 
   protected def putInternal(str: CharSequence, value: Long, overwrite: Boolean): Long = {
     _puts += 1
@@ -219,26 +224,35 @@ abstract class StringDictionaryBase(initialCapacity: Int = 1024, val loadFactor:
   }
 
   protected def putInternalToPos(str: CharSequence, inlinedStr: Long, pos: Int, value: Long, overwrite: Boolean): Long = {
-    if (stringLength(assoc, pos) > 0) { // key `str` is already present
+    if (isAlive(assoc, pos)) { // key `str` is already present
       if (overwrite) {
         setPayload(assoc, pos, value)
         value
       } else {
         payload(assoc, pos)
       }
-    } else { // empty slot
+    } else { // empty slot or removed slot that used to have the same key
+      // this is checked early because alive bit is overwritten by setInlinedWord
+      val isrem = isRemoved(assoc, pos)
+
       if (inlinedStr != 0xffffffffffffffffL) {
         setInlinedWord(assoc, pos, inlinedStr)
         setInlined(assoc, pos)
-        setStringLength(assoc, pos, str.length)
+        setInlinedStringLength(assoc, pos, str.length)
 
       } else {
         val offset = putString(str)
         setStringOffset(assoc, pos, offset)
-        setStringLength(assoc, pos, str.length)
+        setPackedStringLength(assoc, pos, str.length)
       }
+      if (!isrem) {
+        occupied += 1
+      } else {
+        removed -= 1
+        _reputs += 1
+      }
+      setAlive(assoc, pos)
       setPayload(assoc, pos, value)
-      occupied += 1
 
       // grow must be called after insertion of the new element
       // otherwise the `pos` returned by findPos is no longer valid
@@ -298,7 +312,7 @@ abstract class StringDictionaryBase(initialCapacity: Int = 1024, val loadFactor:
     //var h = 0
     //var i = 0
     //while (i < length) {
-    //  h = 31 * h + decode(inlinedChar(assoc, inlinedStr, i))
+    //  h = 31 * h + decode(inlinedChar(inlinedStr, i))
     //  i += 1
     //}
     //h
@@ -312,29 +326,27 @@ abstract class StringDictionaryBase(initialCapacity: Int = 1024, val loadFactor:
     }
 
   protected def inlinedToString(assoc: Array[Int], pos: Int) = {
-    val len = stringLength(assoc, pos)
+    val len = inlinedStringLength(assoc, pos)
     val word = inlinedWord(assoc, pos)
     val arr = new Array[Char](len)
     var i = 0
     while (i < len) {
-      arr(i) = decode(inlinedChar(assoc, word, i))
+      arr(i) = decode(inlinedChar(word, i))
       i += 1
     }
     new String(arr)
   }
 
   protected def charsToString(assoc: Array[Int], pos: Int) = {
-    val len = stringLength(assoc, pos)
+    val len = packedStringLength(assoc, pos)
     val off = stringOffset(assoc, pos)
     new String(chars, off, len)
   }
 
-  /** Returns a position of slot with the specified key or the first empty slot,
-   * ie. the first slot where the requested pair is or where it could be
-   * inserted. */
+  /** Returns a position of a slot with the specified key or the first empty
+    * slot or removed slot that used to have the specified key. ie. the first
+    * slot where the requested pair is located or where it could be inserted. */
   protected def findPos(str: CharSequence, inlined: Long) = {
-    require(str.length != 0 && str.length < 256, "string must be non empty and shorter than 256 chars")
-
     val mask = capacity - 1
 
     if (inlined != 0xffffffffffffffffL) {
@@ -362,42 +374,68 @@ abstract class StringDictionaryBase(initialCapacity: Int = 1024, val loadFactor:
 
   }
 
-  // pos refers to the first element in segment
-  protected def stringLength(assoc: Array[Int], pos: Int) = assoc(pos+1) & 0xff
+  // pos refers to the first element in a segment
+  //
+  // bit patterns (ordered from 31th bit to 0th bit)
+  //
+  // empty           = 0, 0, 30x 0
+  // packed          = 0, 1, 30 bits length
+  // inlined         = 1, 1, 22 bits chars, 8 bits length
+  // removed packed  = 0, 0, 30 bits length
+  // removed inlined = 1, 0, 22 bits chars, 8 bits length
+  //
+  // zero length strings will always be encoded as inlined strings
+  protected def isEmpty  (assoc: Array[Int], pos: Int) = assoc(pos+1) == 0
+  protected def isRemoved(assoc: Array[Int], pos: Int) = assoc(pos+1) != 0 && (assoc(pos+1) & (1 << 30)) == 0
+  protected def isAlive  (assoc: Array[Int], pos: Int) =                      (assoc(pos+1) & (1 << 30)) != 0
+  protected def isInlined(assoc: Array[Int], pos: Int) = (assoc(pos+1) & (1 << 31)) != 0
+
+  protected def setEmpty  (assoc: Array[Int], pos: Int) = { assoc(pos) = 0 ; assoc(pos+1) = 0 }
+  protected def setRemoved(assoc: Array[Int], pos: Int) = assoc(pos+1) &= ~(1 << 30)
+  protected def setAlive  (assoc: Array[Int], pos: Int) = assoc(pos+1) |=  (1 << 30)
+  protected def setInlined(assoc: Array[Int], pos: Int) = assoc(pos+1) |=  (1 << 31)
+
+  protected def packedStringLength (assoc: Array[Int], pos: Int) = assoc(pos+1) & ((1 << 30) - 1)
+  protected def inlinedStringLength(assoc: Array[Int], pos: Int) = assoc(pos+1) & ((1 <<  8) - 1)
   protected def stringOffset(assoc: Array[Int], pos: Int) = assoc(pos)
-  protected def inlinedWord(assoc: Array[Int], pos: Int): Long = (assoc(pos).toLong & 0xffffffffL) | ((assoc(pos+1).toLong & 0x3fffff00L) << 24)
 
-  protected def inlinedChar(assoc: Array[Int], word: Long, i: Int): Int = ((word >>> (6 * i)) & ((1 << 6) - 1)).toInt
-  protected def isInlined(assoc: Array[Int], pos: Int) = (assoc(pos+1) & (1 << 30)) != 0
+  protected def setPackedStringLength (assoc: Array[Int], pos: Int, length: Int) =
+    assoc(pos+1) = assoc(pos+1) | length
+  protected def setInlinedStringLength(assoc: Array[Int], pos: Int, length: Int) =
+    assoc(pos+1) = (assoc(pos+1) & 0xffffff00) | length
+  protected def setStringOffset(assoc: Array[Int], pos: Int, offset: Int) =
+    assoc(pos) = offset
 
-  protected def setStringLength(assoc: Array[Int], pos: Int, length: Int) = assoc(pos+1) = (assoc(pos+1) & 0xffffff00) | length
-  protected def setStringOffset(assoc: Array[Int], pos: Int, offset: Int) = assoc(pos) = offset
-  protected def setInlinedWord(assoc: Array[Int], pos: Int, word: Long) = {
-    assoc(pos) = (word & 0xffffffff).toInt
-    assoc(pos+1) = (word >>> 24).toInt
+  protected def inlinedWord(assoc: Array[Int], pos: Int): Long =
+    (assoc(pos).toLong & 0xffffffffL) | ((assoc(pos+1).toLong & 0x3fffff00L) << 24)
+  protected def inlinedChar(word: Long, i: Int): Int =
+    ((word >>> (6 * i)) & ((1 << 6) - 1)).toInt
+
+  // overwrites inlined/alive bits and length
+  protected def setInlinedWord(assoc: Array[Int], pos: Int, inlinedStr: Long) = {
+    assoc(pos)   = (inlinedStr & 0xffffffff).toInt
+    assoc(pos+1) = (inlinedStr >>> 24).toInt
   }
-  private def setInlinedChar(word: Long, i: Int, value: Int): Long = {
+  private def setInlinedChar(inlinedStr: Long, i: Int, value: Int): Long = {
     //require(value < 64)
-    word | (value.toLong << (6 * i))
+    inlinedStr | (value.toLong << (6 * i))
   }
-  protected def setInlined(assoc: Array[Int], pos: Int) = assoc(pos+1) |= (1 << 30)
 
   protected def payload(assoc: Array[Int], pos: Int): Long
   protected def setPayload(assoc: Array[Int], pos: Int, value: Long): Unit
 
 
   protected def equalOrEmptyInlined(pos: Int, str: CharSequence, inlinedStr: Long): Boolean = {
-    val len = stringLength(assoc, pos)
-    if (len == 0) return true
-    if (!isInlined(assoc, pos)) return false
-    if (len != str.length) return false
-    inlinedWord(assoc, pos) == inlinedStr
+    val inl   = isInlined(assoc, pos)
+
+    isEmpty(assoc, pos) || (inl && inlinedWord(assoc, pos) == inlinedStr)
   }
 
+  /** In this case removed elements are ignored */
   protected def equalOrEmptyNotInlined(pos: Int, str: CharSequence): Boolean = {
-    val len = stringLength(assoc, pos)
-    if (len == 0) return true
+    if (isEmpty(assoc, pos)) return true
     if (isInlined(assoc, pos)) return false
+    val len = packedStringLength(assoc, pos)
     if (len != str.length) return false
 
     val off = stringOffset(assoc, pos)
@@ -439,7 +477,7 @@ abstract class StringDictionaryBase(initialCapacity: Int = 1024, val loadFactor:
       arr(i) = -1
       i += 1
     }
-    i = 0
+    i = 1
 
     ch = '0' ; while (ch <= '9') { arr(ch) = i.toByte ; ch += 1 ; i += 1 }
     ch = 'A' ; while (ch <= 'Z') { arr(ch) = i.toByte ; ch += 1 ; i += 1 }
@@ -451,9 +489,9 @@ abstract class StringDictionaryBase(initialCapacity: Int = 1024, val loadFactor:
   protected def encode(ch: Char): Int = encodeTable(ch).toInt
 
   protected def decode(x: Int): Char = {
-    if (x < 10) return (x      + '0').toChar
-    if (x < 36) return (x - 10 + 'A').toChar
-    /*if (x <= 62)*/ return (x - 36 + 'a').toChar
+    if (x < 10+1) return (x-1      + '0').toChar
+    if (x < 36+1) return (x-1 - 10 + 'A').toChar
+    /*if (x <= 62+1)*/ return (x-1 - 36 + 'a').toChar
   }
 
 
@@ -464,33 +502,47 @@ abstract class StringDictionaryBase(initialCapacity: Int = 1024, val loadFactor:
     this.capacity = capacity * 2
     this.maxOccupied = capacity * loadFactor toInt
 
+    var newOccupied = 0
+
     var oldPos = 0
     while (oldPos < (oldCapacity * segmentLength)) {
-      val len = stringLength(oldAssoc, oldPos)
-      if (len > 0) {
-        val hash = if (isInlined(oldAssoc, oldPos)) {
+      if (isAlive(oldAssoc, oldPos)) {
+        val inl = isInlined(oldAssoc, oldPos)
+
+        val hash = if (inl) {
           inlinedHashCode(inlinedWord(oldAssoc, oldPos))
         } else {
+          val len = packedStringLength(oldAssoc, oldPos)
           charArrHashCode(chars, stringOffset(oldAssoc, oldPos), len)
         }
+
         val mask = capacity - 1
         var i = hash & mask
         var pos = i * segmentLength
-        while (stringLength(assoc, pos) != 0) { // search for space in newly allocated array
+        while (!isEmpty(assoc, pos)) { // search for space in newly allocated array
           i = (i + 1) & mask
           pos = i * segmentLength
           _growProbes += 1
         }
 
+        // copy segment
         var j = 0
         while (j < segmentLength) {
           assoc(pos+j) = oldAssoc(oldPos+j)
           j += 1
         }
+
+        newOccupied += 1
+      } else if (isRemoved(oldAssoc, oldPos)) {
+        // currently removing of packed string causes memory leak
+        // this abhorent negligence will be rectified in the near future
       }
+
       oldPos += segmentLength
     }
 
+    this.occupied = newOccupied
+    this.removed = 0
   }
 
 
