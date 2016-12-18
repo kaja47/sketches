274e1537020f77614c68aa8d97f123be3a2812b9
kaja47 <kaja47@k47.cz>
2016-12-18 05:06:52 +0100

    IndexResultBuilder

--- LSHResultBuilders.scala
+++ LSHResultBuilders.scala
@@ -2,10 +2,10 @@ package atrox.sketch
 
 import atrox.Cursor2
 import atrox.Bits
-import atrox.TopKFloatInt
+import atrox.{ TopKFloatInt, TopKFloatIntEstimate, BruteForceTopKFloatInt }
 
 object IndexResultBuilder {
-  def make(distinct: Boolean, maxResults: Int): IndexResultBuilder = 
+  def make(distinct: Boolean, maxResults: Int): IndexResultBuilder =
     if (maxResults == Int.MaxValue) {
       new AllIndexResultBuilder(distinct)
     } else {
@@ -19,6 +19,7 @@ trait IndexResultBuilder {
   def ++= (rb: IndexResultBuilder): Unit
   def result: Array[Int]
   def idxSimCursor: Cursor2[Int, Float]
+  def minLowerBound: Float
 }
 
 class AllIndexResultBuilder(distinct: Boolean) extends IndexResultBuilder {
@@ -54,11 +55,12 @@ class AllIndexResultBuilder(distinct: Boolean) extends IndexResultBuilder {
     def key   = Bits.unpackIntHi(arr(pos))
     def value = Bits.unpackFloatLo(arr(pos))
   }
+  def minLowerBound: Float = Float.MinValue
 }
 
 class TopKIndexResultBuilder(k: Int, distinct: Boolean) extends IndexResultBuilder {
-  private var res: TopKFloatInt = null // top-k is allocated only when it's needed
-  private def createTopK() = if (res == null) res = new TopKFloatInt(k, distinct)
+  private var res: BruteForceTopKFloatInt = null // top-k is allocated only when it's needed
+  private def createTopK() = if (res == null) res = new BruteForceTopKFloatInt(k)
 
   def size = if (res == null) 0 else res.size
 
@@ -77,4 +79,29 @@ class TopKIndexResultBuilder(k: Int, distinct: Boolean) extends IndexResultBuild
 
   def result = if (res == null) Array() else res.drainToArray()
   def idxSimCursor = { createTopK() ; res.cursor.swap }
+  def minLowerBound: Float = if (res == null || res.size != k) Float.MinValue else res.minKey
+}
+
+class TopKEstimateIndexResultBuilder(k: Int) extends IndexResultBuilder {
+  private var res: TopKFloatIntEstimate = null // top-k is allocated only when it's needed
+  private def createTopK() = if (res == null) res = ??? // new TopKFloatIntEstimate(k, 3)
+
+  def size = if (res == null) 0 else res.size
+
+  def += (idx: Int, sim: Double): Unit = {
+    createTopK()
+    res.insert(sim.toFloat, idx)
+  }
+  def ++= (rb: IndexResultBuilder): Unit = rb match {
+    case rb: TopKEstimateIndexResultBuilder =>
+      if (rb.res != null) {
+        createTopK()
+        res ++= rb.res
+      }
+    case _ => sys.error("this should never happen")
+  }
+
+  def result = if (res == null) Array() else res.drainToArray()
+  def idxSimCursor = { createTopK() ; res.cursor.swap }
+  def minLowerBound: Float = if (res == null) Float.MinValue else res.keyThreshold
 }
