88db554a041a69fb0cdeb97b14c573dcb3e792c2
kaja47 <kaja47@k47.cz>
2016-12-26 06:49:41 +0100

    MinHash, WeigtedMinHash and SingleBitMinHash merged to one object

--- SketchImpls.scala
+++ SketchImpls.scala
@@ -8,62 +8,34 @@ import breeze.stats.distributions.Rand
 
 object MinHash {
 
-  def apply[T](hashFunctions: Int)(implicit mk: HashFunc[Int] => MinHasher[T]): IntSketchers[T] =
-    Sketchers(hashFunctions, (i: Int) => mk(HashFunc.random(i*1000)), Estimator(hashFunctions))
+  def apply[T](hashFunctions: Int)(implicit mk: ((HashFunc[Int], Any => Int)) => WeightedMinHasher[T]): IntSketchers[T] =
+    weighted(hashFunctions, (a: Any) => 1)
 
+  /** based on https://www.sumologic.com/2015/10/22/rapid-similarity-search-with-weighted-min-hash/ */
+  def weighted[T, El](hashFunctions: Int, weights: El => Int)(implicit mk: ((HashFunc[Int], El => Int)) => WeightedMinHasher[T]): IntSketchers[T] =
+    Sketchers(hashFunctions, (i: Int) => mk(HashFunc.random(i*1000), weights), Estimator(hashFunctions))
 
-  trait MinHasher[-T] extends IntSketcher[T] {
-    def apply(t: T): Int
-  }
+  /** MinHash that uses only one bit. It's much faster than traditional MinHash
+    * but it seems it's less precise.
+    * As of right now it's not really suitable for LSH, because most elements
+    * are hashed into few buckets. Investigation pending.
+    * https://www.endgame.com/blog/minhash-vs-bitwise-set-hashing-jaccard-similarity-showdown */
+  def singleBit[T](hashFunctions: Int)(implicit mk: ((HashFunc[Int], Any => Int)) => WeightedMinHasher[T]): BitSketchers[T] =
+    singleBitWeighted(hashFunctions, (a: Any) => 1)
 
-  implicit class IntArrayMinHasher(f: HashFunc[Int]) extends MinHasher[Array[Int]] {
-    def apply(set: Array[Int]): Int = {
-      var min = Int.MaxValue
-      var j = 0 ; while (j < set.length) {
-        min = math.min(min, f(set(j)))
-        j += 1
-      }
-      min
-    }
-  }
+  def singleBitWeighted[T, El](hashFunctions: Int, weights: El => Int)(implicit mk: ((HashFunc[Int], El => Int)) => WeightedMinHasher[T]): BitSketchers[T] =
+    Sketchers(hashFunctions, (i: Int) => SingleBitMinHasher(mk(HashFunc.random(i*1000), weights)), SingleBitEstimator(hashFunctions))
 
-  implicit class GeneralMinHasher[T](f: HashFunc[Int]) extends MinHasher[Traversable[T]] {
-    def apply(set: Traversable[T]): Int = {
-      var min = Int.MaxValue
-      for (el <- set) {
-        min = math.min(min, f(el.hashCode))
-      }
-      min
-    }
-  }
-
-  case class Estimator(sketchLength: Int) extends IntEstimator {
-    def estimateSimilarity(sameBits: Int): Double =
-      sameBits.toDouble / sketchLength
-
-    def minSameBits(sim: Double): Int = {
-      require(sim >= 0.0 && sim <= 1.0, "similarity must be from (0, 1)")
-      (sim * sketchLength).toInt
-    }
-  }
-
-}
-
-
-
-/** based on https://www.sumologic.com/2015/10/22/rapid-similarity-search-with-weighted-min-hash/ */
-object WeightedMinHash {
-
-  def apply[T, W](hashFunctions: Int, weights: W)(implicit mk: ((HashFunc[Int], W)) => WeightedMinHasher[T]): IntSketchers[T] =
-    Sketchers(hashFunctions, (i: Int) => mk(HashFunc.random(i*1000), weights), MinHash.Estimator(hashFunctions))
-
-
-  /** applies one function to every element of one set and reduces it to minumum */
   trait WeightedMinHasher[-T] extends IntSketcher[T] {
     def apply(t: T): Int
   }
 
-  implicit class IntArrayMinHasher(fw: (HashFunc[Int], Array[Int])) extends WeightedMinHasher[Array[Int]] {
+
+  case class SingleBitMinHasher[T](mh: WeightedMinHasher[T]) extends BitSketcher[T] {
+    def apply(x: T): Boolean = (mh(x) & 1) != 0
+  }
+
+  implicit class IntArrayMinHasher(fw: (HashFunc[Int], Int => Int)) extends WeightedMinHasher[Array[Int]] {
     val (f, weights) = fw
     def apply(set: Array[Int]): Int = {
       var min = Int.MaxValue
@@ -80,7 +52,7 @@ object WeightedMinHash {
     }
   }
 
-  implicit class GeneralMinHasher[T](fw: (HashFunc[Int], Map[T, Int])) extends WeightedMinHasher[Traversable[T]] {
+  implicit class GeneralMinHasher[T](fw: (HashFunc[Int], T => Int)) extends WeightedMinHasher[Traversable[T]] {
     val (f, weights) = fw
     def apply(set: Traversable[T]): Int = {
       var min = Int.MaxValue
@@ -95,36 +67,18 @@ object WeightedMinHash {
     }
   }
 
-}
 
+  case class Estimator(sketchLength: Int) extends IntEstimator {
+    def estimateSimilarity(sameBits: Int): Double =
+      sameBits.toDouble / sketchLength
 
-
-/** MinHash that uses only one bit. It's much faster than traditional MinHash
-  * but it seems it's less precise.
-  * As of right now it's not really suitable for LSH, because most elements
-  * are hashed into few buckets. Investigation pending.
-  * https://www.endgame.com/blog/minhash-vs-bitwise-set-hashing-jaccard-similarity-showdown */
-object WeightedSingleBitMinHash {
-  import MinHash.MinHasher
-
-  def apply[T, W](hashFunctions: Int, weights: W)(implicit mk: HashFunc[Int] => MinHasher[T]): BitSketchers[T] =
-    Sketchers(hashFunctions, (i: Int) => WeightedSingleBitMinHasher(mk(HashFunc.random(i*1000))), mkEstimator(hashFunctions))
-
-  case class WeightedSingleBitMinHasher[T](mh: MinHasher[T]) extends BitSketcher[T] {
-    def apply(x: T): Boolean = (mh(x) & 1) != 0
+    def minSameBits(sim: Double): Int = {
+      require(sim >= 0.0 && sim <= 1.0, "similarity must be from (0, 1)")
+      (sim * sketchLength).toInt
+    }
   }
 
-  implicit class IntArrayMinHasher(f: HashFunc[Int]) extends MinHash.IntArrayMinHasher(f)
-  implicit class GeneralMinHasher(f: HashFunc[Int])  extends MinHash.GeneralMinHasher(f)
-
-  def mkEstimator(sketchLength: Int) = sketchLength match {
-    case 64  => new Estimator(sketchLength) with BitEstimator64
-    case 128 => new Estimator(sketchLength) with BitEstimator128
-    case 256 => new Estimator(sketchLength) with BitEstimator256
-    case _   => new Estimator(sketchLength)
-  }
-
-  class Estimator(val sketchLength: Int) extends BitEstimator {
+  case class SingleBitEstimator(val sketchLength: Int) extends BitEstimator {
     def estimateSimilarity(sameBits: Int): Double =
       1.0 - 2.0 / sketchLength * (sketchLength - sameBits)
 
