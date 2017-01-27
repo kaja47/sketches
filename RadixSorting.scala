f093630ba02f89733adefe6c049c93ae9add6365
kaja47 <kaja47@k47.cz>
2017-01-27 02:30:56 +0100

    BurstSort/RadixQuicksort - simplification

--- RadixSorting.scala
+++ RadixSorting.scala
@@ -9,101 +9,71 @@ abstract class RadixElement[S] {
   val classTag: ClassTag[S]
   /** 0-255 or -1 as terminal symbol */
   def byteAt(str: S, pos: Int): Int
-  def sort(arr: Array[S], len: Int, depth: Int): Unit
+  def sort(arr: Array[S], len: Int, depth: Int): Unit =
+    RadixQuicksort.sort(arr, 0, len, depth)(this)
 }
 
 
 object RadixElement {
 
   implicit val Strings: RadixElement[String] = new RadixElement[String] {
-    def byteAt(str: String, pos: Int): Int =
-      if (pos >= str.length*2) -1 else (str.charAt(pos/2) >> ((~pos & 1) * 8)) & 0xff
-
-    def sort(arr: Array[String], len: Int, depth: Int): Unit =
-      RadixQuicksort.sort(arr, 0, len, depth)
-
+    def byteAt(str: String, pos: Int): Int = if (pos >= str.length*2) -1 else (str.charAt(pos/2) >> ((~pos & 1) * 8)) & 0xff
     val classTag: ClassTag[String] = implicitly[ClassTag[String]]
   }
 
   val ASCIIStrings: RadixElement[String] = new RadixElement[String] {
-    def byteAt(str: String, pos: Int): Int =
-      if (pos >= str.length) -1 else str.charAt(pos) & 0xff
-
-    def sort(arr: Array[String], len: Int, depth: Int): Unit =
-      RadixQuicksort.sort(arr, 0, len, depth)
-
+    def byteAt(str: String, pos: Int): Int = if (pos >= str.length) -1 else str.charAt(pos) & 0xff
     val classTag: ClassTag[String] = implicitly[ClassTag[String]]
   }
 
-  implicit val UnsignedByteArrays: RadixElement[Array[Byte]] = new RadixElement[Array[Byte]] { self =>
-    def byteAt(str: Array[Byte], pos: Int): Int =
-      if (pos < str.length) str(pos) & 0xff else -1
-
-    def sort(arr: Array[Array[Byte]], len: Int, depth: Int): Unit =
-      RadixQuicksort.sort[Array[Byte]](arr, 0, len, depth)(self)
-      //Arrays.sort(arr, 0, len, byteArrayComparator)
-
+  implicit val UnsignedByteArrays: RadixElement[Array[Byte]] = new RadixElement[Array[Byte]] {
+    def byteAt(str: Array[Byte], pos: Int): Int = if (pos < str.length) str(pos) & 0xff else -1
     val classTag: ClassTag[Array[Byte]] = implicitly[ClassTag[Array[Byte]]]
   }
 
-  val SignedByteArrays: RadixElement[Array[Byte]] = new RadixElement[Array[Byte]] { self =>
-    def byteAt(str: Array[Byte], pos: Int): Int =
-      if (pos < str.length) (str(pos)+0x80) & 0xff else -1
-
-    def sort(arr: Array[Array[Byte]], len: Int, depth: Int): Unit =
-      RadixQuicksort.sort[Array[Byte]](arr, 0, len, depth)(self)
-
+  val SignedByteArrays: RadixElement[Array[Byte]] = new RadixElement[Array[Byte]] {
+    def byteAt(str: Array[Byte], pos: Int): Int = if (pos < str.length) (str(pos)+0x80) & 0xff else -1
     val classTag: ClassTag[Array[Byte]] = implicitly[ClassTag[Array[Byte]]]
   }
 
   implicit val IntArrays: RadixElement[Array[Int]] = new RadixElement[Array[Int]] {
     def byteAt(str: Array[Int], pos: Int): Int = if (pos >= str.length*4) -1 else ((str(pos/4)+0x80000000) >> (3-(pos%4) * 8)) & 0xff
-    def sort(arr: Array[Array[Int]], len: Int, depth: Int): Unit = RadixQuicksort.sort(arr, 0, len, depth)
     val classTag: ClassTag[Array[Int]] = implicitly[ClassTag[Array[Int]]]
   }
 
   implicit val LongArrays: RadixElement[Array[Long]] = new RadixElement[Array[Long]] {
     def byteAt(str: Array[Long], pos: Int): Int = if (pos >= str.length*8) -1 else (((str(pos/8)+0x8000000000000000L) >> (7-(pos%8) * 8)) & 0xff).toInt
-    def sort(arr: Array[Array[Long]], len: Int, depth: Int): Unit = RadixQuicksort.sort(arr, 0, len, depth)
     val classTag: ClassTag[Array[Long]] = implicitly[ClassTag[Array[Long]]]
   }
 
-
   implicit val Ints: RadixElement[Int] = new RadixElement[Int] {
     def byteAt(str: Int, pos: Int): Int = if (pos >= 4) -1 else ((str+0x80000000) >>> ((3-pos) * 8)) & 0xff
-    def sort(arr: Array[Int], len: Int, depth: Int): Unit = RadixQuicksort.sort(arr, 0, len, depth)
     val classTag: ClassTag[Int] = implicitly[ClassTag[Int]]
   }
 
   implicit val Longs: RadixElement[Long] = new RadixElement[Long] {
     def byteAt(str: Long, pos: Int): Int = if (pos >= 8) -1 else (((str+0x8000000000000000L) >>> ((7-pos) * 8)) & 0xff).toInt
-    def sort(arr: Array[Long], len: Int, depth: Int): Unit = RadixQuicksort.sort(arr, 0, len, depth)
     val classTag: ClassTag[Long] = implicitly[ClassTag[Long]]
   }
 
   implicit val Floats: RadixElement[Float] = new RadixElement[Float] {
     def byteAt(str: Float, pos: Int): Int = if (pos >= 4) -1 else ((Bits.floatToSortableInt(str)+0x80000000) >>> ((3-pos) * 8)) & 0xff
-    def sort(arr: Array[Float], len: Int, depth: Int): Unit = RadixQuicksort.sort(arr, 0, len, depth)
     val classTag: ClassTag[Float] = implicitly[ClassTag[Float]]
   }
 
   implicit val Doubles: RadixElement[Double] = new RadixElement[Double] {
     def byteAt(str: Double, pos: Int): Int = if (pos >= 8) -1 else (((Bits.doubleToSortableLong(str)+0x8000000000000000L) >>> ((7-pos) * 8)) & 0xff).toInt
-    def sort(arr: Array[Double], len: Int, depth: Int): Unit = RadixQuicksort.sort(arr, 0, len, depth)
     val classTag: ClassTag[Double] = implicitly[ClassTag[Double]]
   }
 
-
-  class Mapped[T, S](f: T => S)(implicit res: RadixElement[S], clt: ClassTag[T]) extends RadixElement[T] { self =>
+  class Mapped[T, S](f: T => S)(implicit res: RadixElement[S], clt: ClassTag[T]) extends RadixElement[T] {
     val classTag: ClassTag[T] = clt
     def byteAt(str: T, pos: Int): Int = res.byteAt(f(str), pos)
-    def sort(arr: Array[T], len: Int, depth: Int): Unit = RadixQuicksort.sort(arr, 0, len, depth)(self)
   }
 
   def Mapped[T, S](f: T => S)(implicit els: RadixElement[S], clt: ClassTag[T]) = new Mapped[T, S](f)
 
-
-  implicit def Options[T](implicit el: RadixElement[T]) = new RadixElement[Option[T]] { self =>
+  implicit def Options[T](implicit el: RadixElement[T]) = new RadixElement[Option[T]] {
     val classTag: ClassTag[Option[T]] = implicitly[ClassTag[Option[T]]]
     def byteAt(str: Option[T], pos: Int): Int =
       if (pos == 0) {
@@ -111,10 +81,9 @@ object RadixElement {
       } else {
         el.byteAt(str.get, pos-1)
       }
-    def sort(arr: Array[Option[T]], len: Int, depth: Int): Unit = RadixQuicksort.sort(arr, 0, len, depth)(self)
   }
 
-  implicit def Eithers[A, B](implicit ela: RadixElement[A], elb: RadixElement[B]) = new RadixElement[Either[A, B]] { self =>
+  implicit def Eithers[A, B](implicit ela: RadixElement[A], elb: RadixElement[B]) = new RadixElement[Either[A, B]] {
     val classTag: ClassTag[Either[A, B]] = implicitly[ClassTag[Either[A, B]]]
     def byteAt(str: Either[A, B], pos: Int): Int =
       if (pos == 0) {
@@ -125,7 +94,6 @@ object RadixElement {
           case Right(x) => elb.byteAt(x, pos-1)
         }
       }
-    def sort(arr: Array[Either[A, B]], len: Int, depth: Int): Unit = RadixQuicksort.sort(arr, 0, len, depth)(self)
   }
 
 
